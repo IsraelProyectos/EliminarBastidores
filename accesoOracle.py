f7934895f8c03097740279e82cfb3952582284f5
IsraelProyectos <israelpuyol@hotmail.com>
2017-09-23 17:47:16 +0200

    Add files via upload

--- accesoOracle.py
+++ accesoOracle.py
@@ -3,21 +3,22 @@ import cx_Oracle
 
 class connectToOracle():
 
-	def __init__(self,query,connectionString):
+	def __init__(self,connectionString,textAreaBastidores):
 		self.query=query
 		self.connectionString=connectionString
 	def connect(self):
 		try:	   
 			con =  cx_Oracle.connect(self.connectionString)
 			#print(con.version)
-			cur = con.cursor()
-			cur.execute(self.query)
+			#cur = con.cursor()
+			#cur.execute(self.query)
 			#cur.execute("select * from sko_exec_transactions")
 			#for result in cur:
 				#print(result)
-			return cur
-			cur.close()
-			con.close()
+			#return cur
+			#cur.close()
+			#con.close()
+
 		except cx_Oracle.DatabaseError as e:
 			error, = e.args
 			if error.code == 24373:
