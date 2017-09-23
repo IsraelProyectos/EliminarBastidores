6de22432431ad8e09f49ca16b09d9672abf41417
IsraelProyectos <israelpuyol@hotmail.com>
2017-09-23 17:45:20 +0200

    Add files via upload

--- eliminar_bastidores.py
+++ eliminar_bastidores.py
@@ -1,42 +1,26 @@
+#!/usr/bin/env python
+#coding=utf-8
 import wx
 import accesoOracle
+class MyFrame(wx.Frame):
+    """ Una clase personalizada de frame """
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(400,200))
+        # Control de texto multilínea
+        self.txt = wx.TextCtrl(self, pos=(0,0), size=(100,20))
+        self.control = wx.TextCtrl(self, pos=(0,55), style=wx.TE_MULTILINE)
+        self.buttonTextArea = wx.Button(self, label="hola", pos=(0, 100), size=(50,50))
+        self.buttonTextArea.Bind(wx.EVT_BUTTON, self.OnbuttonTextArea)
+        connectionString='WORK_SKO/WORK_SKO@bvn002b.bbdo.local/PRDBATCH'
+        self.Show(True)
 
-class MainWindow(wx.Frame):
-    def __init__(self, *args, **kwargs):
-        wx.Frame.__init__(self, *args, **kwargs)
-
-        self.panel = wx.Panel(self)
-        self.label = wx.StaticText(self.panel, label="Introduce la query....", pos=(0,0))
-        self.txtQuery = wx.TextCtrl(self.panel, value=" ", size=(140,-1), pos=(0,30))
-
-        #self.button = wx.Button(self.panel, label='Ejecutar borrado', size=(100, 100))
-
-        self.button = wx.Button(self.panel, label="click Me", pos=(0,30))
-        self.connectionString='WORK_SKO/WORK_SKO@bvn002b.bbdo.local/PRDBATCH'
-        
-        self.sizer = wx.BoxSizer()
-        self.sizer.Add(self.label, 1)
-        self.sizer.Add(self.button)
-
-        self.button.Bind(wx.EVT_BUTTON, self.OnButton)
-
-        self.panel.SetSizerAndFit(self.sizer)  
-        self.Show()
-
-    def OnButton(self, e):
-
-    	ao=accesoOracle.connectToOracle(self.txtQuery.GetValue(), self.connectionString)
-    	
-    	if ao is not None:
-    		self.txtQuery.SetValue("correcto")
-
-    	else:
-    		self.txtQuery.SetValue("incorrecto")
-
-        #self.label.SetLabel("Oh, this is very looooong!")
-        #self.sizer.Layout()
-        # self.panel.Layout()  #Either works
+    def OnbuttonTextArea(self, e):
+        textAreaBastidores = self.control.GetValue()
+        #listaBastidores = [texto.split()]
+        #self.txt.SetValue(textAreaBastidores.split())
+        #print(textAreaBastidores.split())
+        ao=accesoOracle.connectToOracle(self.connectionString, textAreaBastidores)
 
 app = wx.App(False)
-win = MainWindow(None)
+frame = MyFrame(None, 'Editor simple')
 app.MainLoop()
